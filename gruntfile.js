bbe4ebfdda4c86b0e275ab16fbe85214b0105ca2
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2023-01-29 16:52:52 -0300

    Ate a aula 5

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    "dev/styles/main.css": "src/styles/main.less"
+                }
+            },
+            production: {
+                files: {
+                    "dist/styles/main.min.css": "src/styles/main.less"
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less')
+
+    grunt.registerTask("default", ["less:development"])
+    grunt.registerTask("build", ["less:production"])
+}
