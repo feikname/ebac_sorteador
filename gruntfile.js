bbf2bbe7685909809fb08646c92beac923a5202e
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2023-01-30 10:57:44 -0300

    aula watch

--- gruntfile.js
+++ gruntfile.js
@@ -12,11 +12,18 @@ module.exports = function (grunt) {
                     "dist/styles/main.min.css": "src/styles/main.less"
                 }
             }
-        }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development'],
+            },
+        },
     })
 
     grunt.loadNpmTasks('grunt-contrib-less')
+    grunt.loadNpmTasks('grunt-contrib-watch')
 
-    grunt.registerTask("default", ["less:development"])
+    grunt.registerTask("default", ["watch"])
     grunt.registerTask("build", ["less:production"])
 }
