388223aaedc5cfd406041f69a2c371eaf4220b05
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2023-01-30 11:56:07 -0300

    grunt-replace, grunt-contrib-htmlmin e grunt-clean
    E mais do layout da pagina

--- gruntfile.js
+++ gruntfile.js
@@ -8,6 +8,9 @@ module.exports = function (grunt) {
                 }
             },
             production: {
+                options: {
+                    compress: true
+                },
                 files: {
                     "dist/styles/main.min.css": "src/styles/main.less"
                 }
@@ -18,12 +21,69 @@ module.exports = function (grunt) {
                 files: ['src/styles/**/*.less'],
                 tasks: ['less:development'],
             },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
         },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.html' // antes do replace
+                }
+            }
+        },
+        clean: ['prebuild']
     })
 
     grunt.loadNpmTasks('grunt-contrib-less')
     grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-replace')
+    grunt.loadNpmTasks('grunt-contrib-htmlmin')
+    grunt.loadNpmTasks('grunt-contrib-clean')
 
     grunt.registerTask("default", ["watch"])
-    grunt.registerTask("build", ["less:production"])
+    grunt.registerTask("build", ["less:production", "htmlmin:dist", "replace:dist", "clean"])
 }
